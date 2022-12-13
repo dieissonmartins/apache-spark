ab3daad5dda4840a1f27170a83f2be7efddc7e52
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-12-13 20:26:42 -0300

    teste DataFrames

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 full_path = os.path.dirname(os.path.realpath(__file__))
+full_path_spark = full_path + '/spark/'
 
 # spark-version: spark-3.3.1-bin-hadoop3
 os.environ["SPARK_HOME"] = full_path + "/spark/"
@@ -14,6 +15,11 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .master('local[*]') \
     .appName("Iniciando com Spark") \
+    .config('spark.ui.port', '4050') \
     .getOrCreate()
 
-print(spark)
+# spark is an existing SparkSession
+df = spark.read.json(full_path_spark + "examples/src/main/resources/people.json")
+
+# Displays the content of the DataFrame to stdout
+df.show()
