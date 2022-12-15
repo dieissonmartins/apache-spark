6d6e4e3053b75e6bbb14b9264da01346ae85ed9a
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-12-15 20:36:11 -0300

    mostra table com toPandas()

--- analysis.py
+++ analysis.py
@@ -23,15 +23,21 @@ path_companies = full_path + '/tmp/empresas'
 companies = spark.read.csv(path_companies, sep=';', inferSchema=True)
 companies_count = companies.count()
 print('Quantidade de empresas: ' + str(companies_count))
+companies_pandas = companies.limit(10).toPandas()
+print(companies_pandas)
 
 # dados socios
 path_partners = full_path + '/tmp/socios'
 partners = spark.read.csv(path_partners, sep=';', inferSchema=True)
 partners_count = partners.count()
 print('Quantidade de socios: ' + str(partners_count))
+partners_pandas = partners.limit(10).toPandas()
+print(partners_pandas)
 
 # dados estabelecimentos
 path_establishments = full_path + '/tmp/estabelecimentos'
 establishments = spark.read.csv(path_establishments, sep=';', inferSchema=True)
 establishments_count = establishments.count()
 print('Quantidade de estabelecimentos: ' + str(establishments_count))
+establishments_pandas = establishments.limit(10).toPandas()
+print(establishments_pandas)
