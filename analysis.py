de66d219651b6146a0354ae7f8648381b7ca74d4
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-12-16 11:57:17 -0300

    nomeada colunas de empresas e convertida coluna "capital_social_da_empresa" para DoubleType

--- analysis.py
+++ analysis.py
@@ -11,6 +11,8 @@ import findspark
 findspark.init()
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType, StringType
+from pyspark.sql import functions as f
 
 spark = SparkSession.builder \
     .master('local[*]') \
@@ -20,27 +22,56 @@ spark = SparkSession.builder \
 
 # dados da empresas
 path_companies = full_path + '/tmp/empresas'
+
+# carrega dados do csv
 companies = spark.read.csv(path_companies, sep=';', inferSchema=True)
+
+# quanlidade de dados
 companies_count = companies.count()
 print('Quantidade de empresas: ' + str(companies_count))
+
+# tipagem das conlunas
 companies.printSchema()
-companies_pandas = companies.limit(100).toPandas()
-print(companies_pandas)
+
+# consulta dados com limite de 100 linhas
+companies = companies.limit(10)
+
+# define nome para as colunas
+companies_col_names = [
+    'cnpj_basico',
+    'razao_social_nome_empresarial',
+    'natureza_juridica',
+    'qualificacao_do_responsavel',
+    'capital_social_da_empresa',
+    'porte_da_empresa',
+    'ente_federativo_responsavel'
+]
+
+# renomea colunas
+for index, col_name in enumerate(companies_col_names):
+    companies = companies.withColumnRenamed(f"_c{index}", col_name)
+
+companies = companies.withColumn('capital_social_da_empresa', f.regexp_replace('capital_social_da_empresa', ',', '.'))
+companies = companies.withColumn('capital_social_da_empresa', companies['capital_social_da_empresa'].cast(DoubleType()))
+
+# imprime o dataframe de empresas
+print(companies.toPandas())
+companies.printSchema()
 
 # dados socios
-path_partners = full_path + '/tmp/socios'
-partners = spark.read.csv(path_partners, sep=';', inferSchema=True)
-partners_count = partners.count()
-print('Quantidade de socios: ' + str(partners_count))
-partners.printSchema()
-partners_pandas = partners.limit(100).toPandas()
-print(partners_pandas)
+# path_partners = full_path + '/tmp/socios'
+# partners = spark.read.csv(path_partners, sep=';', inferSchema=True)
+# partners_count = partners.count()
+# print('Quantidade de socios: ' + str(partners_count))
+# partners.printSchema()
+# partners_pandas = partners.limit(100).toPandas()
+# print(partners_pandas)
 
 # dados estabelecimentos
-path_establishments = full_path + '/tmp/estabelecimentos'
-establishments = spark.read.csv(path_establishments, sep=';', inferSchema=True)
-establishments_count = establishments.count()
-establishments.printSchema()
-print('Quantidade de estabelecimentos: ' + str(establishments_count))
-establishments_pandas = establishments.limit(100).toPandas()
-print(establishments_pandas)
+# path_establishments = full_path + '/tmp/estabelecimentos'
+# establishments = spark.read.csv(path_establishments, sep=';', inferSchema=True)
+# establishments_count = establishments.count()
+# establishments.printSchema()
+# print('Quantidade de estabelecimentos: ' + str(establishments_count))
+# establishments_pandas = establishments.limit(100).toPandas()
+# print(establishments_pandas)
