526699fdfa481962def24186c59e1f38aeb670c2
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-04-26 19:13:08 -0300

    AVG notas matematica sexo feminino

--- microdados-enem.py
+++ microdados-enem.py
@@ -23,3 +23,45 @@ spark = SparkSession.builder \
 # baixe no arquivo Microdados do Enem 2020 na pasta temp
 # https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enem
 
+filePath = full_path + '/tmp/MICRODADOS_ENEM_2020.csv'
+
+# Leitura de dados
+microdados_enem = (
+    spark
+    .read
+    .format('csv')
+    .option("delimiter", ";")
+    .option("header", True)
+    .option("inferSchema", True)
+    .load(filePath)
+)
+
+# ver estrutura
+#microdados_enem.printSchema()
+
+# view dados
+#microdados_enem.show()
+
+# iniciar uma view sql
+microdados_enem.createOrReplaceTempView('microdados_enem')
+
+
+spark.sql("""
+    SELECT 
+         TP_SEXO
+        ,CO_PROVA_CN
+        ,CO_PROVA_CH
+        ,CO_PROVA_LC
+        ,CO_PROVA_MT
+        ,NU_NOTA_CN
+        ,NU_NOTA_CH
+        ,NU_NOTA_LC
+        ,NU_NOTA_MT
+    FROM microdados_enem
+""").show()
+
+
+# média da nota em matemática apenas para as alunas do sexo Feminino
+spark.sql("""
+SELECT AVG(NU_NOTA_MT) FROM microdados_enem WHERE TP_SEXO = 'F'
+""").show()
